aa379b5898711eca51d6867adf09e2f9a961a50e
yaoshicheng <929239151@qq.com>
2019-01-11 07:02:21 +0800

    Add files via upload

---
+++ src/mock/login.js
@@ -0,0 +1,36 @@
+import { param2Obj } from '@/utils'
+
+const userMap = {
+  admin: {
+    roles: ['admin'],
+    token: 'admin',
+    introduction: '我是管理员',
+    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    name: 'Super Admin'
+  },
+  editor: {
+    roles: ['editor'],
+    token: 'editor',
+    introduction: '我是运营',
+    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    name: 'Normal Editor'
+  }
+}
+
+export default {
+  loginByUsername: config => {
+    JSON.parse(config.body);
+    // const { mobile } = JSON.parse(config.body);
+    // 根据手机号获取用户权限
+    return userMap["admin"]
+  },
+  getUserInfo: config => {
+    const { token } = param2Obj(config.url)
+    if (userMap[token]) {
+      return userMap[token]
+    } else {
+      return false
+    }
+  },
+  logout: () => 'success'
+}
