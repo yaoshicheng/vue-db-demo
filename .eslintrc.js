2cb5d70b3d1ea0d989483be68313da6a2c517a9f
yaoshicheng <929239151@qq.com>
2019-01-11 07:28:58 +0800

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential',
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'eslint:recommended'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules:{
+    "no-console": "error",
+    "no-debugger": "off",
+    "no-control-regex": "off",
+    "no-useless-escape": "off"
+  }
+};
