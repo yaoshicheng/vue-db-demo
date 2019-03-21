dd51fb6da09610446b647f607079b68e5b3bdb59
yaoshicheng <929239151@qq.com>
2019-03-21 14:25:01 +0800

    update config

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -51,9 +51,18 @@ module.exports = {
         loader: 'babel-loader',
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
+     {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+        include: [resolve('src/icons')],
+        options: {
+          symbolId: 'icon-[name]'
+        }
+      },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
+        exclude: [resolve('src/icons')],
         options: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
